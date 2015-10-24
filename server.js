4cab91aa0af84aefe23b94918d00f142d40c2f48
Josh <hauuguu@gmail.com>
2015-10-24 19:44:44 -0400

    Added email authentication in server.js

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ var app = express();
 
 var User = require('./models/user.js');
 
+var nodemailer = require("nodemailer");
+
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "hackunilol",
+        pass: "unilol2015"
+    }
+});
+	//COLLAPSED FOR SECURITY	
+	
+//=========================================================================
+	
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 
@@ -24,6 +41,62 @@ require('./routes.js')(app);
 //ROUTES
 
 var router = express.Router(); 
+
+
+//====================== EMAIL AUTHENTICATION ======================
+	
+var rand,mailOptions,host,link;
+
+/*------------------SMTP Over-----------------------------*/
+
+/*------------------Routing Started ------------------------*/
+
+app.get('/send',function(req,res){
+        rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : req.query.to,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+	});
+});
+
+app.get('/verify',function(req,res){
+console.log(req.protocol+":/"+req.get('host'));
+if((req.protocol+"://"+req.get('host'))==("http://"+host))
+{
+    console.log("Domain is matched. Information is from Authentic email");
+    if(req.query.id==rand)
+    {
+        console.log("email is verified");
+        res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+    }
+    else
+    {
+        console.log("email is not verified");
+        res.end("<h1>Bad Request</h1>");
+    }
+}
+else
+{
+    res.end("<h1>Request is from unknown source");
+}
+});
+
+
+//====================== END OF EMAIL AUTHENTICATION ======================
+
 
 // test route to make sure everything is working (accessed at GET http://localhost:3000/api)
 router.get('/', function(req, res) {
