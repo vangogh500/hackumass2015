ed405a14ee4eedd5cfabd5504977ab28ea6c54c3
Kai Matsuda <kaisanomatsuda@1x-vl906-128-119-238-248.wireless.umass.edu>
2015-10-25 04:56:19 -0400

    handling errors for registration

--- public/js/controllers/SchoolController.js
+++ public/js/controllers/SchoolController.js
@@ -1,5 +1,21 @@
 app.controller('SchoolController', function($scope, $routeParams){
 	console.log($routeParams.schoolID);
+	$http({
+	  method: 'GET',
+	  url: '/send/' + $scope.email + '/' + $scope.username + '/' + $scope.ign
+	}).then(function successCallback(response) {
+		if(response.data == 'error') {
+			$scope.msg = "Email was invalid!";
+	    	$scope.$apply;
+		}
+		else {
+			$scope.msg = "Email was sent!";
+			$scope.$apply;
+		}
+	}, function errorCallback(response) {
+	    $scope.msg = "Oops! Something went wrong contacting the server!";
+	    $scope.$apply;
+	});
 	d3.json('data/' + $routeParams.schoolID + '.json', function(data) {
 		$scope.players = data;
 		$scope.$apply();
--- public/js/controllers/Sign-InController.js
+++ public/js/controllers/Sign-InController.js
@@ -1,18 +1,41 @@
 app.controller('Sign-InController', function($scope, $routeParams, $http){
 	console.log('test');
+	function validateEmail(email) {
+    	var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    	return re.test(email);
+	}
 	$scope.verify = function() {
-		$http({
-		  method: 'POST',
-		  url: '/api/user/' + $scope.username + '/' + $scope.ign
-		}).then(function successCallback(response) {
-			console.log(response);
-		    // this callback will be called asynchronously
-		    // when the response is available
-		  }, function errorCallback(response) {
-		    // called asynchronously if an error occurs
-		    // or server returns response with an error status.
-		    console.log(response);
-		});
-		
+		if($scope.username && $scope.ign && $scope.email) {
+			if(!validateEmail($scope.email)) {
+				$scope.msg = $scope.email + " is not a valid email!";
+				$scope.$apply;
+			}
+			else if(($scope.email).substr(($scope.email).length - 4) != ".edu") {
+				$scope.msg = $scope.email + " is not a .edu email!";
+				$scope.$apply;
+			}
+			else {
+				$http({
+				  method: 'GET',
+				  url: '/send/' + $scope.email + '/' + $scope.username + '/' + $scope.ign
+				}).then(function successCallback(response) {
+					if(response.data == 'error') {
+						$scope.msg = "Email was invalid!";
+				    	$scope.$apply;
+					}
+					else {
+						$scope.msg = "Email was sent!";
+						$scope.$apply;
+					}
+				}, function errorCallback(response) {
+				    $scope.msg = "Oops! Something went wrong contacting the server!";
+				    $scope.$apply;
+				});
+			}
+		}
+		else {
+			$scope.msg = "Make sure that all fields are filled in!";
+			$scope.$apply;
+		}
 	};
 });
