2128d2c7ce38ce240a6e9d42f9363119a705a0e3
Josh <hauuguu@gmail.com>
2015-10-25 03:35:16 -0400

    Changed email verification

--- routes.js
+++ routes.js
@@ -33,6 +33,66 @@ module.exports = function(app) {
 
     /*------------------Routing Started ------------------------*/
 
+    app.get('/send/:email', function(req, res) {
+        rand = Math.floor((Math.random() * 100) + 54);
+        host = req.get('host');
+        link = "http://" + req.get('host') + "/verify?id=" + rand;
+        mailOptions = {
+            to: req.params.email,
+            subject: "Please confirm your Email account",
+            html: "Hello,<br> Please Click on the link to verify your email.<br><a href=" + link + ">Click here to verify</a>"
+        }
+        console.log(mailOptions);
+        smtpTransport.sendMail(mailOptions, function(error, response) {
+            if (error) {
+                console.log(error);
+                res.end("error");
+            } else {
+                console.log("Message sent: " + response.message);
+                res.end("sent");
+            }
+        });
+    });
+
+    app.get('/verify', function(req, res) {
+        console.log(req.protocol + ":/" + req.get('host'));
+        if ((req.protocol + "://" + req.get('host')) == ("http://" + host)) {
+            console.log("Domain is matched. Information is from Authentic email");
+            if (req.query.id == rand) {
+                console.log("email is verified");
+                res.end("<h1>Email " + mailOptions.to + " is been Successfully verified");
+            } else {
+                console.log("email is not verified");
+                res.end("<h1>Bad Request</h1>");
+            }
+        } else {
+            res.end("<h1>Request is from unknown source");
+        }
+    });
+
+
+    //====================== END OF EMAIL AUTHENTICATION ======================	
+
+	//====================== MASTERY PAGE AUTHENTICATION ======================
+
+    var rand, mailOptions, host, link;
+	
+	/*
+	Here we are configuring our SMTP Server details.
+	STMP is mail server which is responsible for sending and recieving email.
+	*/
+	var smtpTransport = nodemailer.createTransport("SMTP", {
+		service: "Gmail",
+		auth: { //COLLAPSED FOR SECURITY
+			user: "hackunilol",
+			pass: "unilol2015"
+		}
+	});
+
+    /*------------------SMTP Over-----------------------------*/
+
+    /*------------------Routing Started ------------------------*/
+
     app.get('/send', function(req, res) {
         rand = Math.floor((Math.random() * 100) + 54);
         host = req.get('host');
@@ -71,8 +131,8 @@ module.exports = function(app) {
     });
 
 
-    //====================== END OF EMAIL AUTHENTICATION ======================	
-
+    //====================== END OF MASTERY PAGE AUTHENTICATION ======================	
+	
 
     //get, deletes, creates a new user, and edits a user
 
@@ -91,11 +151,6 @@ module.exports = function(app) {
 			if (err) return res.send(500, 'Error occurred: database error');
 			else res.send('Delete success!');
 		});
-	
-		/*UserJS.remove({ loginUser: req.params.username}, function(err){
-			if (err) return res.send(500, 'Error occurred: database error');
-			else res.send('Delete success!');
-		}*/
 	});
 	
 	app.post('/api/user/:username/:ign', function(req, res) {
